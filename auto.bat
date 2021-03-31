3fd69451628e8c125f23f329d268de9015d4cf98
fernandor2 <fernanmolon@hotmail.es>
2021-03-31 02:50:43 +0200

    añadido encoder

--- auto.bat
+++ auto.bat
@@ -2,5 +2,5 @@
 set/p video= Video Name: 
 cls
 set/p timep= Part duration (in seconds): 
-python ffmpeg-split.py -f "%video%.mp4" -s %timep%
+python ffmpeg-split.py -f "%video%.mp4" -s %timep% -v h264
 exit
